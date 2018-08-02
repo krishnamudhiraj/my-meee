4e51a87b372a5c921d8c5783a8153751614f9fab
Dhirendra Pratap <dhirendra@geekyants.com>
2018-08-02 19:45:25 +0530

    Values components and its sub component added

--- src/screens/Home/ValuesSetion.js
+++ src/screens/Home/ValuesSetion.js
@@ -1,12 +1,14 @@
 import React, { Component } from 'react';
 import TextSection from '../../components/TextSection';
+import ButtonTransparent from '../../components/ButtonTransparent';
+import CardGetStarted from './CardGetStarted';
 
 class ValuesSetion extends Component {
   render() {
     return (
       <div className="values-section">
         <div className="container">
-          <div className="row">
+          <div className="row values-section-row">
             <div className="col-sm-7">
               <TextSection
                 title="What are your values?"
@@ -17,7 +19,12 @@ class ValuesSetion extends Component {
                 body="All too often we live on autopilot. So this is your chance to stop and think about what really matters to you. In just a few minutes, you’ll begin to see yourself – and your future – in a new light."
               />
             </div>
-            <div className="col-sm-5" />
+            <div className="col-sm-5">
+              <div className="btn-card-view">
+                <CardGetStarted />
+                <ButtonTransparent text="Already have a code?" />
+              </div>
+            </div>
           </div>
         </div>
       </div>
