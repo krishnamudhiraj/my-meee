ae8ac76931fd7cfd16a3e158ff03f5e5ccfbd6b4
Krishna MS <krishna@Krishnas-Air-2.blr-office.geekyants.com>
2018-10-08 17:52:56 +0530

    storing cards data

--- pages/OrderCards/index.js
+++ pages/OrderCards/index.js
@@ -12,134 +12,131 @@ import Loader from '../../components/Loader';
 import withAuth from '../../utils/functions/withAuth';
 
 class OrderCards extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      warningMessage: '',
-      maxCount: props.myTopFiveCards.length,
-      ranks: [],
-    };
-    this.onRankChange = this.onRankChange.bind(this);
-    this.onNextClick = this.onNextClick.bind(this);
-  }
+	constructor(props) {
+		super(props);
+		this.state = {
+			warningMessage: '',
+			maxCount: props.myTopFiveCards.length,
+			ranks: [],
+		};
+		this.onRankChange = this.onRankChange.bind(this);
+		this.onNextClick = this.onNextClick.bind(this);
+	}
 
-  onRankChange = rankObject => {
-    const { ranks } = this.state;
-    let flag = true;
-    ranks.forEach(element => {
-      if (element.cardNumber === rankObject.cardNumber) {
-        element.rank = rankObject.rank;
-        flag = false;
-      }
-    });
-    if (flag) this.setState({ ranks: ranks.concat(rankObject) });
-  };
+	onRankChange = rankObject => {
+		const { ranks } = this.state;
+		let flag = true;
+		ranks.forEach(element => {
+			if (element.cardNumber === rankObject.cardNumber) {
+				element.rank = rankObject.rank;
+				flag = false;
+			}
+		});
+		if (flag) this.setState({ ranks: ranks.concat(rankObject) });
+	};
 
-  UNSAFE_componentWillReceiveProps(nextProps) {
-    if (
-      nextProps.isLoading === false &&
-      nextProps.myTopFiveCardsRanking !== []
-    ) {
-      Router.push('/DownloadPDF');
-    }
-  }
+	UNSAFE_componentWillReceiveProps(nextProps) {
+		if (nextProps.isLoading === false && nextProps.myTopFiveCardsRanking !== []) {
+			Router.push('/DownloadPDF');
+		}
+	}
 
-  onNextClick = () => {
-    const { ranks, maxCount } = this.state;
-    const { onCardSubmission } = this.props;
-    let nextVisible = true;
-    let rankConf = [];
-    _.map(ranks, item => {
-      rankConf.push(item.rank);
-    });
-    if (_.uniq(rankConf).length !== rankConf.length) {
-      this.setState({ warningMessage: 'Please order all the cards.' });
-      setTimeout(() => {
-        this.setState({ warningMessage: '' });
-      }, 2000);
-    } else {
-      if (ranks.length === maxCount) {
-        ranks.forEach(element => {
-          if (element.rank === '') {
-            nextVisible = false;
-          }
-        });
-      } else {
-        nextVisible = false;
-      }
+	onNextClick = () => {
+		const { ranks, maxCount } = this.state;
+		const { onCardSubmission } = this.props;
+		let nextVisible = true;
+		let rankConf = [];
+		_.map(ranks, item => {
+			rankConf.push(item.rank);
+		});
+		if (_.uniq(rankConf).length !== rankConf.length) {
+			this.setState({ warningMessage: 'Please order all the cards.' });
+			setTimeout(() => {
+				this.setState({ warningMessage: '' });
+			}, 2000);
+		} else {
+			if (ranks.length === maxCount) {
+				ranks.forEach(element => {
+					if (element.rank === '') {
+						nextVisible = false;
+					}
+				});
+			} else {
+				nextVisible = false;
+			}
 
-      if (nextVisible) {
-        onCardSubmission(ranks);
-      } else {
-        this.setState({ warningMessage: 'Please order all the cards.' });
-        setTimeout(() => {
-          this.setState({ warningMessage: '' });
-        }, 2000);
-      }
-    }
-  };
+			if (nextVisible) {
+				onCardSubmission(ranks);
+			} else {
+				this.setState({ warningMessage: 'Please order all the cards.' });
+				setTimeout(() => {
+					this.setState({ warningMessage: '' });
+				}, 2000);
+			}
+		}
+	};
 
-  render() {
-    const { isLoading, myTopFiveCards } = this.props;
-    const { maxCount, ranks, warningMessage, errorMessage } = this.state;
-    return (
-      <div className="values-card">
-        {isLoading && <Loader bg loadingText={'Saving your Cards...'} />}
-        <CardsScreenLayout
-          header={
-            <CardsScreenHeader
-              onBackPress={'/OrderCardsIntro'}
-              // forwardText="Finish"
-              // onForwardPress={'/'}
-            />
-          }
-          body={
-            <FiveCardList
-              titleText={{
-                title: 'My Top 5',
-                body:
-                  'These are your Top 5 cards. To order them, just add the number in the box next to them, with 1 being the most important to you, and 5 being the least',
-              }}
-              data={myTopFiveCards}
-              bodyView={item => (
-                <CardRank
-                  key={item.card_no}
-                  maxCount={maxCount}
-                  ranks={ranks}
-                  data={item}
-                  onRankChange={this.onRankChange}
-                />
-              )}
-              nextButton={true}
-              nextButtonText={{
-                text: 'Next step',
-                bottomText: 'Submit your ranking',
-              }}
-              nextButtonAlign={'flex-end'}
-              onNextButton={this.onNextClick}
-              warning={warningMessage}
-            />
-          }
-        />
-      </div>
-    );
-  }
+	render() {
+		const { isLoading, myTopFiveCards } = this.props;
+		const { maxCount, ranks, warningMessage, errorMessage } = this.state;
+		return (
+			<div className="values-card">
+				{isLoading && <Loader bg loadingText={'Saving your Cards...'} />}
+				<CardsScreenLayout
+					header={
+						<CardsScreenHeader
+							onBackPress={'/OrderCardsIntro'}
+							// forwardText="Finish"
+							// onForwardPress={'/'}
+						/>
+					}
+					body={
+						<FiveCardList
+							titleText={{
+								title: 'My Top 5',
+								body:
+									'These are your Top 5 cards. To order them, just add the number in the box next to them, with 1 being the most important to you, and 5 being the least',
+							}}
+							data={myTopFiveCards}
+							bodyView={item => (
+								<CardRank
+									key={item.card_no}
+									maxCount={maxCount}
+									ranks={ranks}
+									data={item}
+									onRankChange={this.onRankChange}
+								/>
+							)}
+							nextButton={true}
+							nextButtonText={{
+								text: 'Next step',
+								bottomText: 'Submit your ranking',
+							}}
+							nextButtonAlign={'flex-end'}
+							onNextButton={this.onNextClick}
+							warning={warningMessage}
+						/>
+					}
+				/>
+			</div>
+		);
+	}
 }
 
 const mapDispatchToProps = dispatch => ({
-  dispatch,
-  onCardSubmission: item => dispatch(onCardSubmission(item)),
+	dispatch,
+	onCardSubmission: item => dispatch(onCardSubmission(item)),
 });
 
 const mapStateToProps = ({ valueCards }) => ({
-  isLoading: get(valueCards, 'isLoading'),
-  myTopFiveCards: get(valueCards, 'myTopFiveCards'),
-  myTopFiveCardsRanking: get(valueCards, 'myTopFiveCardsRanking'),
+	isLoading: get(valueCards, 'isLoading'),
+	myTopFiveCards: get(valueCards, 'myTopFiveCards'),
+	myTopFiveCardsRanking: get(valueCards, 'myTopFiveCardsRanking'),
 });
 
 export default withAuth(
-  connect(
-    mapStateToProps,
-    mapDispatchToProps
-  )(OrderCards)
+	connect(
+		mapStateToProps,
+		mapDispatchToProps
+	)(OrderCards)
 );
--- services/schools.js
+++ services/schools.js
@@ -3,95 +3,135 @@ import { firebaseApp } from '../config/firebase';
 const db = firebaseApp.firestore();
 
 export const updateUserData = (key, data) =>
-  new Promise((resolve, reject) => {
-    db.collection('/schoolsUsers')
-      .doc(key)
-      .update(data)
-      .then(() => {
-        resolve(true);
-      })
-      .catch(err => {
-        reject(err);
-      });
-  });
+	new Promise((resolve, reject) => {
+		db.collection('/schoolsUsers')
+			.doc(key)
+			.update(data)
+			.then(() => {
+				resolve(true);
+			})
+			.catch(err => {
+				reject(err);
+			});
+	});
+
+export const updateSchoolUserData = (key, data) =>
+	// const datas = await getData(data);
+	new Promise((resolve, reject) => {
+		db.collection('/schoolsUsers')
+			.doc(key)
+			.set(
+				{
+					cardsData: data,
+				},
+				{
+					merge: true,
+				}
+			)
+			.then(() => {
+				resolve(true);
+			})
+			.catch(err => {
+				reject(err);
+			});
+	});
+
+// async function getData(udata) {
+// 	const data = await Promise.all(
+// 		udata.map(async item => {
+// 			const response = await delete item.cardNumber;
+// 			return item;
+// 		})
+// 	);
+// 	return data;
+// }
 
 export const getCode = () => {
-  const codes = fetchCodes();
-  let flag = true;
-  let randomNumber = null;
+	const codes = fetchCodes();
+	let flag = true;
+	let randomNumber = null;
 
-  while (flag) {
-    randomNumber = Math.floor(1000 + Math.random() * 9000);
-    if (!has(codes, randomNumber)) {
-      flag = false;
-    }
-  }
+	while (flag) {
+		randomNumber = Math.floor(1000 + Math.random() * 9000);
+		if (!has(codes, randomNumber)) {
+			flag = false;
+		}
+	}
 
-  return randomNumber;
+	return randomNumber;
 };
 
 export const fetchCodes = () => {
-  const codes = [];
+	const codes = [];
 
-  db.collection('/schools').get().then(snapshot => {
-    snapshot.forEach(doc => {
-      const data = doc.data();
-      codes.push(data.code);
-    });
-  }).catch(err => {
-    console.log('Error getting codes data: ', err);
-  });
+	db.collection('/schools')
+		.get()
+		.then(snapshot => {
+			snapshot.forEach(doc => {
+				const data = doc.data();
+				codes.push(data.code);
+			});
+		})
+		.catch(err => {
+			console.log('Error getting codes data: ', err);
+		});
 
-  return codes;
+	return codes;
 };
 
-export const generateSchoolCodes = (data) => new Promise((resolve, reject) => {
-  const code = getCode();
-  const item = {
-    school_name: data.school,
-    code,
-    is_code_used: false,
-    allowed_users: Number(data.numberOfCodes)
-  };
-  db.collection('/schools')
-    .add(item)
-    .catch(err => {
-      reject({ response: false, error: err });
-      console.log('Error while generating codes: ', err);
-    });
-  resolve({ response: true, codes: code });
-});
+export const generateSchoolCodes = data =>
+	new Promise((resolve, reject) => {
+		const code = getCode();
+		const item = {
+			school_name: data.school,
+			code,
+			is_code_used: false,
+			allowed_users: Number(data.numberOfCodes),
+		};
+		db.collection('/schools')
+			.add(item)
+			.catch(err => {
+				reject({ response: false, error: err });
+				console.log('Error while generating codes: ', err);
+			});
+		resolve({ response: true, codes: code });
+	});
 
-export const fetchSchools = () => new Promise((resolve, reject) => {
-  let schools = [];
+export const fetchSchools = () =>
+	new Promise((resolve, reject) => {
+		let schools = [];
 
-  db.collection('/schools').get().then(snapshot => {
-    snapshot.forEach(doc => {
-      const data = doc.data();
-      schools.push(data.school_name);
-    });
-    schools = uniq(schools);
-    resolve(schools);
-  }).catch(err => {
-    console.log('Error getting school names: ', err);
-    reject(err);
-  });
-});
+		db.collection('/schools')
+			.get()
+			.then(snapshot => {
+				snapshot.forEach(doc => {
+					const data = doc.data();
+					schools.push(data.school_name);
+				});
+				schools = uniq(schools);
+				resolve(schools);
+			})
+			.catch(err => {
+				console.log('Error getting school names: ', err);
+				reject(err);
+			});
+	});
 
-export const fetchSchoolDataService = (schoolname) => new Promise((resolve, reject) => {
-  const schoolData = [];
-  db.collection('/schoolsUsers')
-    .where('school_name', '==', schoolname)
-    .get()
-    .then(snapshot => {
-      snapshot.forEach(doc => {
-        const data = doc.data();
-        schoolData.push(data);
-      });
-      resolve(schoolData);
-    })
-    .catch(err => {
-      console.log('Error getting school data: ', err);
-      reject(err);
-    });
-});
+export const fetchSchoolDataService = schoolname =>
+	new Promise((resolve, reject) => {
+		const schoolData = [];
+		db.collection('/schoolsUsers')
+			.where('school_name', '==', schoolname)
+			.get()
+			.then(snapshot => {
+				snapshot.forEach(doc => {
+					const data = doc.data();
+					schoolData.push(data);
+				});
+				resolve(schoolData);
+			})
+			.catch(err => {
+				console.log('Error getting school data: ', err);
+				reject(err);
+			});
+	});
